0e364cb8e076234761670e1ee405276c12beba2c
Rafael Cerqueira Gomes <rafahcerqueira@gmail.com>
2024-01-05 18:29:16 -0300

    feat: incluindo paginacao na busca de questions

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -1,6 +1,19 @@
+import { z } from "zod";
 import { JwtAuthGuard } from "@/auth/jwt-auth.guards";
 import { PrismaService } from "@/prisma/prisma.service";
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default("1")
+  .transform(Number)
+  .pipe(z.number().min(1));
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
 
 @Controller("questions")
 @UseGuards(JwtAuthGuard)
@@ -8,8 +21,12 @@ export class FetchQuestionsQuestionController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  async handle() {
+  async handle(@Query("page", queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 1;
+
     const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
       orderBy: {
         createdAt: "desc",
       },
